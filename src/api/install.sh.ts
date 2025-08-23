4d140db7d1716c9ab6924c6f9540c3ba2ad75f8c
orbitaljin <ouzaher.saad@yahoo.fr>
2025-08-23 13:20:05 +0000

    feat: Serve install scripts from API routes

---
+++ src/api/install.sh.ts
@@ -0,0 +1,21 @@
+import type { APIRoute } from 'astro';
+
+const githubUrl = 'https://raw.githubusercontent.com/michiapp/michi/main/install.sh';
+
+export const get: APIRoute = async ({ _ }) => {
+  const res = await fetch(githubUrl);
+
+  if (!res.ok) {
+    return new Response('Failed to fetch install script, try to install it from github instead!', { status: 500 });
+  }
+
+  const text = await res.text();
+
+  return new Response(text, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+      'Cache-Control': 'no-cache',
+    },
+  });
+};
